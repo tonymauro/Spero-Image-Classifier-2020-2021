a18db949af88ad2f3d974032bfe9767383cace5c
Ish-Patel <ishpat17@gmail.com>
2020-11-17 12:21:16 -0800

    Added Comments
    added comments to improve the coherence and explanation of the gapstatisticmethod file

--- CompiledAlgorithms/GapStatisticMethod.py
+++ CompiledAlgorithms/GapStatisticMethod.py
@@ -10,36 +10,47 @@ from sklearn.datasets.samples_generator import make_blobs
 from sklearn.metrics import pairwise_distances
 
 
-
 class Gap:
 
     def __init__(self):
         return
 
-    def getInertia(self,a,X):
+    # Method to compute the inertia for the given cluster and dataset
+    def getInertia(self, a, X):
         z = [np.mean(pairwise_distances(X[a == c, :])) for c in np.unique(a)]
         return np.mean(z)
 
+    # Method to compute the gap statistic of the given (data), with the given clustering (algorithm) ie: KMeans()
+    # (kmax) represents the upper limit of the number of clusters
+    # (nrefs) represents the number of reference data sets that will be created
     def getGap(self, algorithm, data, kmax=11, nrefs=5):
 
+        # conforms extreme inputs
         if len(data.shape) == 1:
-            data = data.reshape(-1,1)
+            data = data.reshape(-1, 1)
+
+        # generates reference data set
         ref = np.random.rand(*data.shape)
-        refInertia = []
-        for k in range (1, kmax+1):
+
+        # computes inertia of reference data set
+        ref_inertia = []
+        for k in range(1, kmax + 1):
             inertia = []
-            for _ in range (nrefs):
+            for _ in range(nrefs):
                 algorithm.n_clusters = k
                 assignments = algorithm.fit_predict(ref)
                 inertia.append(self.getInertia(assignments, ref))
-            refInertia.append(np.mean(inertia))
+            ref_inertia.append(np.mean(inertia))
 
-        dataInertia = []
+        # computes inertia of given data set
+        data_inertia = []
         for k in range(1, kmax):
             algorithm.n_clusters = k
             assignments = algorithm.fit_redict(data)
-            dataInertia.append(self.getInertia(assignments, data))
+            data_inertia.append(self.getInertia(assignments, data))
 
-        gaps = np.log(np.mean(refInertia)) - np.log(dataInertia)
+        # creates an array of gap values for all k, using the gap statistics given formula
+        gaps = np.log(np.mean(ref_inertia)) - np.log(data_inertia)
 
-        return (gaps.argmax() + 1)
+        # returns the index of the largest gap value, representing the determined optimal number of clusters
+        return gaps.argmax() + 1
