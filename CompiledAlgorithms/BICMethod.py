826386e61e030d6402fab514e38ffc03549f409f
Elden Ren <eldenbr2015@gmail.com>
2020-12-18 13:36:07 -0800

    small changes to setup

--- CompiledAlgorithms/BICMethod.py
+++ CompiledAlgorithms/BICMethod.py
@@ -17,7 +17,7 @@ class BIC:
     def customBIC(self, data, origData):
         bics = []
         for n_clusters in self.cluster_range:
-            gmm = GaussianMixture(n_components=n_clusters, covariance_type='full')
+            gmm = GaussianMixture(n_components=n_clusters, n_init=3, covariance_type='full')
             gmm.fit(data)
             clusters = []
             for i in range(n_clusters):
@@ -30,13 +30,13 @@ class BIC:
             log_sigma = np.zeros(n_clusters)
             for i in range(n_clusters):
                 det = np.linalg.det(np.cov(np.transpose(np.array(clusters[i]))))
-                log_sigma[i] = np.log(max(det, 0.0003))
+                log_sigma[i] = np.log(max(det, 0.01))
             bic = 0
             bic += np.dot(clusterSizes, np.log(clusterSizes))
             bic -= np.dot(clusterSizes, log_sigma)/2
             q = n_clusters*(n_clusters+3)/2
             bic -= q*np.sum(np.log(clusterSizes))/2
-            bic -= 2*n_clusters*n_clusters*n_clusters*np.sqrt(n_clusters) #additional penalty term to prevent overenumeration
+            bic -= 4*n_clusters*n_clusters*n_clusters*np.sqrt(n_clusters) #additional penalty term to prevent overenumeration
             print(bic)
             bics.append(bic)
         return self.cluster_range[bics.index(max(bics))]
